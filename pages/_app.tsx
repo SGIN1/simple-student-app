07bf6d4e0562776d1b6ed21e32529f812e1369e4
sklflsdfj <www.sgin3@gmail.com>
2025-06-06 03:15:00 +0300

    تحديث: إضافة جميع التعديلات الأخيرة

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../public/القالب_المشترك.css'; // تأكد من المسار الصحيح لملفات CSS
-import '../public/القالب_المشترك2.css'; // إذا كان لديك أكثر من ملف CSS
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
